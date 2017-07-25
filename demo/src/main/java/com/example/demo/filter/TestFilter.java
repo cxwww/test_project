663b1b043c952da1ea704147aa68735c4902cc01
cxwww <510880341@qq.com>
2017-07-25 17:48:46 +0800

    112

--- demo/src/main/java/com/example/demo/filter/TestFilter.java
+++ demo/src/main/java/com/example/demo/filter/TestFilter.java
@@ -10,12 +10,16 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.core.annotation.Order;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.util.StringUtils;
 
 
 @Order(1)
-@WebFilter(filterName="testFilter", urlPatterns="/*")
+@WebFilter(filterName="testFilter", urlPatterns="/data/shop/*")
 public class TestFilter implements Filter{
 
 	@Override
@@ -28,8 +32,36 @@ public class TestFilter implements Filter{
 	public void doFilter(ServletRequest srequest, ServletResponse sresponse,
 			FilterChain filterChain) throws IOException, ServletException {
 		HttpServletRequest request = (HttpServletRequest) srequest;
+		HttpServletResponse response = (HttpServletResponse) sresponse;
+		HttpSession session = request.getSession(true);
+		
 		System.out.println("this is MyFilter, uri : " + request.getRequestURI());
 		System.out.println("this is MyFilter, url : " + request.getRequestURL());
+		
+		//checkToken
+		String username = (String) session.getAttribute("username");
+		String sessionId = session.getId();
+		if(StringUtils.isEmpty(username) || StringUtils.isEmpty(sessionId)) {
+			response.getWriter().write("username or sessionId is null");
+			response.sendRedirect("/login.jsp");
+			if(!response.isCommitted())
+				filterChain.doFilter(srequest, sresponse);
+			return;
+		}
+		String tokenP = request.getParameter("token");
+//		String token = RedisUtils.getTokenFromRedis(username+"_"+sessionId);
+		//for test
+		StringRedisTemplate srt = (StringRedisTemplate)session.getAttribute("redis");
+		String token = srt.opsForValue().get(username+"_"+sessionId);
+		if(!token.equals(tokenP)) {
+			response.getWriter().write("no authorization");
+			response.sendRedirect("/authorization_error.jsp");
+			if(!response.isCommitted())
+				filterChain.doFilter(srequest, sresponse);
+			return;
+		}
+		
+		
 		filterChain.doFilter(srequest, sresponse);
 	}
 
--- demo/src/test/java/com/example/demo/DataControllerTest.java
+++ demo/src/test/java/com/example/demo/DataControllerTest.java
@@ -1,22 +1,28 @@
 package com.example.demo;
 
+import javax.servlet.http.HttpSession;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import com.alibaba.fastjson.JSONObject;
-import com.example.demo.controller.DataController;
 import com.example.demo.domain.TokenRequestParam;
+import com.example.demo.filter.TestFilter;
+import com.example.demo.util.RedisUtils;
 import com.fasterxml.jackson.core.JsonProcessingException;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -27,29 +33,41 @@ public class DataControllerTest {
 
 	@Autowired
 	WebApplicationContext webApplicationConnect;
-
+	
+	@Autowired
+	StringRedisTemplate srt;
+	
 	@Before
 	public void setUp() throws JsonProcessingException {
-		mvc = MockMvcBuilders.webAppContextSetup(webApplicationConnect).build();
-
+		mvc = MockMvcBuilders.webAppContextSetup(webApplicationConnect).addFilter(new TestFilter(), "/data/shop/*").build();
+//		mvc = MockMvcBuilders.webAppContextSetup(webApplicationConnect).build();
 	}
 
+	
 	@Test
-	public void getTokenTest() throws Exception {
-		mvc.perform(MockMvcRequestBuilders.get("/data/token"))
-				.andDo(MockMvcResultHandlers.print()).andReturn();
-	}
-
-	@Test
-	public void getTokenCTest() throws Exception {
+	public void getPageOverviewTest() throws Exception{
+		//get token
 		TokenRequestParam rParam = new TokenRequestParam();
 		String requestJson = JSONObject.toJSONString(rParam);
 		System.out.println(requestJson);
+		MvcResult result = mvc.perform(
+					MockMvcRequestBuilders.post("/data/tokenC?username=uu")
+							.contentType(MediaType.APPLICATION_JSON)
+							.content(requestJson))
+					.andDo(MockMvcResultHandlers.print()).andReturn();
+		//make some params
+		HttpSession session = result.getRequest().getSession();
+		String username = result.getRequest().getParameter("username");
+		String key = username+"_"+session.getId();
+		String token = srt.opsForValue().get(key);
+		session.setAttribute("username", username);
+		session.setAttribute("redis", srt);
+		//request with same session
 		mvc.perform(
-				MockMvcRequestBuilders.post("/data/tokenC")
-						.contentType(MediaType.APPLICATION_JSON)
-						.content(requestJson))
-				.andDo(MockMvcResultHandlers.print()).andReturn();
+				MockMvcRequestBuilders.get("/data/shop/pageOverview/1/1/1")
+				.param("token", token).session((MockHttpSession) session))
+		.andDo(MockMvcResultHandlers.print()).andReturn();
+		
 	}
 
 }
